301519dad1b6a431c1fbf674a7e65a1b8028bf57
217080 <pavloffme@gmail.com>
2024-09-12 10:30:53 +0300

    First task

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
+
+
+}
+
+enum Gender {
+    MALE,
+    FEMAIL,
+    OTHER
+}
+
+/**
+ * Задание 1: Создайте класс Person с полями name, age, gender.
+ * Реализуйте методы для вывода информации о человеке и увеличения его возраста. Добавьте метод для изменения имени
+ */
+class Person {
+    private String name;
+    private int age;
+    private final Gender gender;
+
+    Person(String name, int age, Gender gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    void printInfo() {
+        System.out.println("ИНФОРИАЦИЯ О " + name);
+        System.out.println("возраст: " + age);
+        System.out.println("пол: " + gender.toString());
+    }
+
+    void changeName(String newName) {
+        name = newName;
+    }
+
+    void plusYearToAge() {
+        age ++;
+    }
+
 }
