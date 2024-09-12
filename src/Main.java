63036448f649962eec00946af9bc624dc2cc32e8
217080 <pavloffme@gmail.com>
2024-09-12 10:45:59 +0300

    Task 4

--- src/Main.java
+++ src/Main.java
@@ -122,3 +122,27 @@ class Cow implements  Animal{
     }
 }
 
+
+/**
+ * Задание 4: Создайте абстрактный класс Transport с абстрактным методом move().
+ * Реализуйте классы Car и Bike, которые наследуются от Transport и реализуют метод move()
+ */
+
+abstract class Transport {
+    abstract void move();
+}
+
+class Car extends Transport {
+    @Override
+    void move() {
+        System.out.println("Машина едет");
+    }
+}
+
+class Bike extends Transport {
+    @Override
+    void move() {
+        System.out.println("Байк едет");
+    }
+}
+
